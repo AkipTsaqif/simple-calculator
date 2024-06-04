c862cbc5d4e020e4dd100ba03b272b7699d35350
Akip Tsaqif <akiptsaqif@gmail.com>
2024-06-04 10:24:16 +0700

    Added more checks to prevent entering operation before operand and compacted the codebase as suggested by Jack

--- script/calc-operation.js
+++ script/calc-operation.js
@@ -1,255 +1,135 @@
 const MAX_DIGITS = 13;
-
 const RESULT = document.getElementById("result");
 const OPS_DISPLAY = document.getElementById("ops-display");
 
-let firstOperation = true;
-let enterPressed = false;
-let currentNumber = 0;
-let resultNumber = 0;
+let firstOperation = true, enterPressed = false;
+let currentNumber = 0, resultNumber = 0;
 let currentOperator = "";
 
-const numberPressed = (num) => {
-    if (num === "00" && currentNumber === 0) return;
-    if (OPS_DISPLAY.textContent === "0") OPS_DISPLAY.textContent = "";
-    else if (
-        OPS_DISPLAY.textContent.match(/[\+\-\*/=]0(?![\.\+\-\*/=])/g) !== null
-    )
-        OPS_DISPLAY.textContent = OPS_DISPLAY.textContent.slice(0, -1);
-
-    OPS_DISPLAY.textContent += num;
-
-    if (currentNumber === 0) currentNumber = num;
-    else currentNumber += num;
+const updateDisplay = (text) => OPS_DISPLAY.textContent += text;
+const setDisplay = (text) => OPS_DISPLAY.textContent = text;
+const resetCalculator = () => {
+    setDisplay("");
+    RESULT.textContent = "0";
+    currentNumber = 0;
+    resultNumber = 0;
+    currentOperator = "";
+    firstOperation = true;
 };
 
-const firstOperationTrue = (operation) => {
+const handleNumber = (num) => {
+    if (enterPressed || resultNumber === Infinity) resetCalculator();
+    if (num === "00" && currentNumber === 0) return;
+    if (OPS_DISPLAY.textContent === "0") setDisplay("");
+    else if (OPS_DISPLAY.textContent.match(/[\+\-\*/=]0(?![\.\+\-\*/=])/g) !== null) 
+        setDisplay(OPS_DISPLAY.textContent.slice(0, -1));
+
+    updateDisplay(num);
+    currentNumber = currentNumber === '0' ? num : currentNumber + num;
+};
+
+const handleFirstOperation = (operation) => {
     resultNumber = currentNumber;
     currentOperator = operation;
-    OPS_DISPLAY.textContent += operation;
+    updateDisplay(operation);
     firstOperation = false;
 };
 
+const handleOperator = (operation) => {
+    if (firstOperation) {
+        handleFirstOperation(operation);
+    } else if (currentNumber !== 0 && currentOperator !== "") {
+        updateDisplay(operation);
+        resultNumber = operate(resultNumber, currentNumber, currentOperator);
+        RESULT.textContent = resultNumber.toString();
+    } else if (currentNumber !== 0) {
+        currentOperator = operation;
+        updateDisplay(operation);
+    } else if (currentNumber === 0)
+        OPS_DISPLAY.textContent = OPS_DISPLAY.textContent.slice(0, -1) + operation;
+    currentOperator = operation;
+    currentNumber = 0;
+    enterPressed = false;
+}
+
 const keyHandlers = {
-    "+": () => {
-        if (firstOperation) {
-            firstOperationTrue("+");
-        } else if (currentNumber !== 0 && currentOperator !== "") {
-            OPS_DISPLAY.textContent += "+";
-            resultNumber = operate(
-                resultNumber,
-                currentNumber,
-                currentOperator
-            );
-            RESULT.textContent = resultNumber.toString();
-        } else if (currentNumber !== 0) {
-            currentOperator = "+";
-            OPS_DISPLAY.textContent += "+";
-        } else if (currentNumber === 0)
-            OPS_DISPLAY.textContent =
-                OPS_DISPLAY.textContent.slice(0, -1) + "+";
-        currentOperator = "+";
-        currentNumber = 0;
-    },
-    "-": () => {
-        if (firstOperation) {
-            firstOperationTrue("-");
-        } else if (currentNumber !== 0 && currentOperator !== "") {
-            OPS_DISPLAY.textContent += "-";
-            resultNumber = operate(
-                resultNumber,
-                currentNumber,
-                currentOperator
-            );
-            RESULT.textContent = resultNumber.toString();
-        } else if (currentNumber !== 0) {
-            currentOperator = "-";
-            OPS_DISPLAY.textContent += "-";
-        } else if (currentNumber === 0)
-            OPS_DISPLAY.textContent =
-                OPS_DISPLAY.textContent.slice(0, -1) + "-";
-        currentOperator = "-";
-        currentNumber = 0;
-    },
-    "*": () => {
-        if (firstOperation) {
-            firstOperationTrue("*");
-        } else if (currentNumber !== 0 && currentOperator !== "") {
-            OPS_DISPLAY.textContent += "*";
-            resultNumber = operate(
-                resultNumber,
-                currentNumber,
-                currentOperator
-            );
-            RESULT.textContent = resultNumber.toString();
-        } else if (currentNumber !== 0) {
-            currentOperator = "*";
-            OPS_DISPLAY.textContent += "*";
-        } else if (currentNumber === 0)
-            OPS_DISPLAY.textContent =
-                OPS_DISPLAY.textContent.slice(0, -1) + "*";
-        currentOperator = "*";
-        currentNumber = 0;
-    },
-    "/": () => {
-        if (firstOperation) {
-            firstOperationTrue("/");
-        } else if (currentNumber !== 0 && currentOperator !== "") {
-            OPS_DISPLAY.textContent += "/";
-            resultNumber = operate(
-                resultNumber,
-                currentNumber,
-                currentOperator
-            );
-            RESULT.textContent = resultNumber.toString();
-        } else if (currentNumber !== 0) {
-            currentOperator = "/";
-            OPS_DISPLAY.textContent += "/";
-        } else if (currentNumber === 0)
-            OPS_DISPLAY.textContent =
-                OPS_DISPLAY.textContent.slice(0, -1) + "/";
-        currentOperator = "/";
-        currentNumber = 0;
-    },
+    "+": () => handleOperator("+"),
+    "-": () => handleOperator("-"),
+    "*": () => handleOperator("*"),
+    "/": () => handleOperator("/"),
     Enter: () => {
         if (currentNumber !== 0 && currentOperator !== "") {
-            resultNumber = operate(
-                resultNumber,
-                currentNumber,
-                currentOperator
-            );
-            OPS_DISPLAY.textContent += `=${resultNumber}`;
+            resultNumber = operate(resultNumber, currentNumber, currentOperator);
+            updateDisplay(`=${resultNumber}`);
             RESULT.textContent = resultNumber.toString();
             currentOperator = "";
         }
+        enterPressed = true;
     },
     Backspace: () => {
-        RESULT.textContent =
-            RESULT.textContent.length > 1
-                ? RESULT.textContent.slice(0, -1)
-                : "0";
-        OPS_DISPLAY.textContent = OPS_DISPLAY.textContent.slice(0, -1);
+        RESULT.textContent = RESULT.textContent.length > 1
+                            ? RESULT.textContent.slice(0, -1)
+                            : "0";
+        setDisplay(OPS_DISPLAY.textContent.slice(0, -1));
         currentNumber = currentNumber.slice(0, -1);
     },
-    Delete: () => {
-        RESULT.textContent = "0";
-        OPS_DISPLAY.textContent = "";
-        currentNumber = 0;
-        resultNumber = 0;
-        currentOperator = "";
-        firstOperation = true;
-    },
+    Delete: () => resetCalculator(),
     ".": () => {
-        if (!currentNumber.includes(".")) {
-            OPS_DISPLAY.textContent += ".";
+        if (enterPressed) {
+            resetCalculator();
+            updateDisplay("0.");
+            currentNumber = "0.";
+            enterPressed = false;
+        } else if (!currentNumber.includes(".")) {
+            updateDisplay(".");
             currentNumber += ".";
         }
     },
     "+/-": () => {
         if (currentNumber !== 0 && resultNumber === 0) {
-            currentNumber =
-                currentNumber < 0
-                    ? Math.abs(currentNumber)
-                    : -Math.abs(currentNumber);
-            OPS_DISPLAY.textContent = currentNumber;
+            currentNumber = currentNumber < 0
+                            ? Math.abs(currentNumber)
+                            : -Math.abs(currentNumber);
+            setDisplay(currentNumber);
         } else if (resultNumber !== 0) {
-            resultNumber =
-                resultNumber < 0
-                    ? Math.abs(resultNumber)
-                    : -Math.abs(resultNumber);
+            resultNumber = resultNumber < 0
+                            ? Math.abs(resultNumber)
+                            : -Math.abs(resultNumber);
             RESULT.textContent = resultNumber;
-            OPS_DISPLAY.textContent = resultNumber;
+            setDisplay(resultNumber);
         }
     },
-    0: () => {
-        numberPressed("0");
-    },
     "00": () => {
-        numberPressed("00");
-    },
-    1: () => {
-        numberPressed("1");
-    },
-    2: () => {
-        numberPressed("2");
-    },
-    3: () => {
-        numberPressed("3");
-    },
-    4: () => {
-        numberPressed("4");
-    },
-    5: () => {
-        numberPressed("5");
-    },
-    6: () => {
-        numberPressed("6");
-    },
-    7: () => {
-        numberPressed("7");
-    },
-    8: () => {
-        numberPressed("8");
-    },
-    9: () => {
-        numberPressed("9");
-    },
-};
-
-const btnHandlers = {
-    add: keyHandlers["+"],
-    substract: keyHandlers["-"],
-    multiply: keyHandlers["*"],
-    divide: keyHandlers["/"],
-    equal: keyHandlers["Enter"],
-    comma: keyHandlers["."],
-    clear: keyHandlers["Delete"],
-    sign: keyHandlers["+/-"],
-    0: keyHandlers["0"],
-    "00": keyHandlers["00"],
-    1: keyHandlers["1"],
-    2: keyHandlers["2"],
-    3: keyHandlers["3"],
-    4: keyHandlers["4"],
-    5: keyHandlers["5"],
-    6: keyHandlers["6"],
-    7: keyHandlers["7"],
-    8: keyHandlers["8"],
-    9: keyHandlers["9"],
-};
-
-for (let key in btnHandlers) {
-    let button = document.getElementById(key);
-    if (button) {
-        button.addEventListener("click", () => {
-            if (resultNumber === Infinity) {
-                btnHandlers["clear"]();
-            } else {
-                if (OPS_DISPLAY.textContent.length === 0 && isNaN(key)) return;
-                else btnHandlers[key]();
-            }
-        });
+        handleNumber("00");
     }
-}
+};
+
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        const key = button.id;
+        if (resultNumber === Infinity) keyHandlers["Delete"]();
+        if (!isNaN(key) && resultNumber !== Infinity) handleNumber(key);
+        if (OPS_DISPLAY.textContent.length === 0 && isNaN(key)) return;
+        else keyHandlers[key]();
+    });
+});
 
 document.addEventListener("keydown", (e) => {
-    if (!isNaN(e.key) && resultNumber !== Infinity) {
+    if (!isNaN(e.key)) {
         e.preventDefault();
-        numberPressed(e.key);
+        handleNumber(e.key);
     } else if (e.key in keyHandlers && resultNumber !== Infinity) {
         e.preventDefault();
-
         if (OPS_DISPLAY.textContent.length === 0) return;
 
         keyHandlers[e.key]();
-
         if (e.key === ".") return;
         if (e.key === "Enter") {
             enterPressed = true;
             return;
         }
         if (!e.key === "Backspace") currentNumber = 0;
-    } else if (resultNumber === Infinity) {
+    } else if (e.key in keyHandlers && resultNumber === Infinity) {
         keyHandlers["Delete"]();
     }
 });
@@ -261,15 +141,9 @@ const operate = (num1, num2, operation) => {
         "*": (a, b) => a * b,
         "/": (a, b) => a / b,
     };
-
     let result = operations[operation](parseFloat(num1), parseFloat(num2));
-    let resultLength = result.toString().replace(".").length;
-
-    if (resultLength > MAX_DIGITS) {
-        let integerPartLength = result.toString().split(".")[0].length;
-        let decimalPlaces = Math.max(0, 13 - integerPartLength);
-        result = parseFloat(result.toFixed(decimalPlaces));
-    }
-
-    return result;
+    let intPartLength = Math.floor(result).toString().length;
+    return intPartLength > MAX_DIGITS 
+        ? result.toExponential(MAX_DIGITS - 4).replace("e+", "e") 
+        : parseFloat(result.toFixed(MAX_DIGITS - intPartLength));
 };
